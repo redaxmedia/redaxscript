6b28d8b1d199a7f36547b2dcffabb150faf29147
redaxmedia <info@redaxmedia.com>
2012-11-20 23:49:57 +0100

    Cache added to dawanda module

--- modules/dawanda/scripts/dawanda.js
+++ modules/dawanda/scripts/dawanda.js
@@ -50,33 +50,54 @@
 
 		r.module.dawanda.getData = function (call, id, page, callback)
 		{
-			/* api request */
+			/* fetch from cache */
 
-			$.ajax({
-				url: r.module.dawanda.getURL(call, id),
-				dataType: 'jsonp',
-				data:
-				{
-					api_key: options.key,
-					page: page || 1
-				},
-				success: function (data)
-				{
-					/* store data */
+			if (typeof r.module.dawanda.data[id] === 'object' && r.module.dawanda.data[id]['calls'][call])
+			{
+				/* callback if data */
 
-					if (typeof data.response === 'object')
+				if (typeof callback === 'function')
+				{
+					callback.call(this);
+				}
+			}
+
+			/* else request data */
+
+			else
+			{
+				$.ajax({
+					url: r.module.dawanda.getURL(call, id),
+					dataType: 'jsonp',
+					cache: true,
+					data:
 					{
-						r.module.dawanda.data[id] = $.extend({}, r.module.dawanda.data[id], data.response.result || {});
+						api_key: options.key,
+						page: page || 1
+					},
+					success: function (data)
+					{
+						/* store data */
 
-						/* callback if data */
-
-						if (typeof callback === 'function')
+						if (typeof data.response === 'object' && typeof data.response.result === 'object')
 						{
-							callback.call(this);
+							r.module.dawanda.data[id] = $.extend({}, r.module.dawanda.data[id], data.response.result || {});
+
+							/* register calls */
+
+							r.module.dawanda.data[id]['calls'] = r.module.dawanda.data[id]['calls'] || {};
+							r.module.dawanda.data[id]['calls'][call] = true;
+
+							/* callback if data */
+
+							if (typeof callback === 'function')
+							{
+								callback.call(this);
+							}
 						}
 					}
-				}
-			});
+				});
+			}
 		};
 	};
 
