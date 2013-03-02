b8bca394d2a140d47c5e59d6612a910a1c8d0708
redaxmedia <info@redaxmedia.com>
2013-03-02 18:01:44 +0100

    Maps module wording

--- modules/maps/scripts/startup.js
+++ modules/maps/scripts/startup.js
@@ -81,6 +81,6 @@ r.module.maps =
 		],
 		replaceBranding: true,
 		mapLogo: '<a href="http://maps.google.com" target="_blank">Google Maps</a>',
-		mapTerms: '<a href="http://www.google.com/intl/en-US_US/help/terms_maps.html" target="_blank">Terms of Use</a>'
+		mapTerms: '<a href="http://www.google.com/intl/en-US_US/help/terms_maps.html" target="_blank">Terms of use</a>'
 	}
 };
