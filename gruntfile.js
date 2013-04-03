6ace4f322ad48630db483af5d852c19a0b81778f
redaxmedia <info@redaxmedia.com>
2013-04-03 23:11:47 +0200

    grunt update finished

--- gruntfile.js
+++ gruntfile.js
@@ -10,7 +10,7 @@ module.exports = function (grunt)
 		{
 			scripts:
 			{
-				files: ['<%=jshint.base%>', '<%=jshint.modules>', '<%=jshint.templates%>'],
+				files: ['<%=jshint.base%>', '<%=jshint.modules%>', '<%=jshint.templates%>'],
 				tasks: ['jshint']
 			},
 			styles:
