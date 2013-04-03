564c06b2d42e34afb168b72a8cade44b97e1ddd7
redaxmedia <info@redaxmedia.com>
2013-04-03 19:34:24 +0200

    grunt updated to 0.4.1

--- modules/maps/config.php
+++ modules/maps/config.php
@@ -3,6 +3,6 @@
 /* config maps */
 
 define(MAPS_API_URL, 'http://maps.googleapis.com/maps/api/js');
-define(MAPS_API_KEY, '');
+define(MAPS_API_KEY, 'AIzaSyApJDayHOmOnVy6OucJXG_cGSHSC_f7NSM');
 define(MAPS_SENSOR, 'true');
 ?>
