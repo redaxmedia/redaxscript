ceb0742a6664619996acb2d7de6104e258da5d03
redaxmedia <info@redaxmedia.com>
2013-04-25 13:37:47 +0200

    Another validator update #2

---
+++ modules/validator/languages/de.php
@@ -0,0 +1,9 @@
+<?php
+
+/* validator */
+
+$l['validator_from'] = 'Von';
+$l['validator_to'] = 'bis';
+$l['validator_line'] = 'Zeile';
+$l['validator_column'] = 'Abschnitt';
+?>
--- modules/validator/languages/en.php
+++ modules/validator/languages/en.php
@@ -2,8 +2,8 @@
 
 /* validator */
 
-$l['validator_from'] = 'from';
+$l['validator_from'] = 'From';
 $l['validator_to'] = 'to';
-$l['validator_line'] = 'line';
-$l['validator_column'] = 'column';
+$l['validator_line'] = 'Line';
+$l['validator_column'] = 'Column';
 ?>
