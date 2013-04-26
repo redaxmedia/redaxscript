fa46e03a71548483d8178d15d5ca0b4786ce61f2
redaxmedia <info@redaxmedia.com>
2013-04-26 13:28:23 +0200

    Disable validator module on admin

--- modules/validator/index.php
+++ modules/validator/index.php
@@ -6,10 +6,13 @@
 
 function validator_loader_start()
 {
-	global $loader_modules_styles, $loader_modules_scripts;
-	$loader_modules_styles[] = 'modules/validator/styles/validator.css';
-	$loader_modules_scripts[] = 'modules/validator/scripts/startup.js';
-	$loader_modules_scripts[] = 'modules/validator/scripts/validator.js';
+	if (FIRST_PARAMETER != 'admin')
+	{
+		global $loader_modules_styles, $loader_modules_scripts;
+		$loader_modules_styles[] = 'modules/validator/styles/validator.css';
+		$loader_modules_scripts[] = 'modules/validator/scripts/startup.js';
+		$loader_modules_scripts[] = 'modules/validator/scripts/validator.js';
+	}
 }
 
 /**
@@ -18,12 +21,15 @@ function validator_loader_start()
 
 function validator_loader_scripts_transport_start()
 {
-	$output = languages_transport(array(
-		'validator_from',
-		'validator_to',
-		'validator_line',
-		'validator_column'
-	));
-	echo $output;
+	if (FIRST_PARAMETER != 'admin')
+	{
+		$output = languages_transport(array(
+			'validator_from',
+			'validator_to',
+			'validator_line',
+			'validator_column'
+		));
+		echo $output;
+	}
 }
 ?>
