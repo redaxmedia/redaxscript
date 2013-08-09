7ebc9446d31f553ee984e70a114fa6307b028cef
redaxmedia <info@redaxmedia.com>
2013-08-09 23:10:21 +0200

    Share this plugin now with popups

--- modules/share_this/index.php
+++ modules/share_this/index.php
@@ -13,8 +13,10 @@
 
 function share_this_loader_start()
 {
-	global $loader_modules_styles;
+	global $loader_modules_styles, $loader_modules_scripts;
 	$loader_modules_styles[] = 'modules/share_this/styles/share_this.css';
+	$loader_modules_scripts[] = 'modules/share_this/scripts/startup.js';
+	$loader_modules_scripts[] = 'modules/share_this/scripts/share_this.js';
 }
 
 /**
@@ -62,14 +64,14 @@ function share_this($route = '')
 	if ($route)
 	{
 		$output = '<div class="wrapper_share_this clear_fix"><ul class="list_share_this">';
-		$output .= '<li>' . anchor_element('external', '', 'link_share_this link_facebook', 'Facebook', 'facebook.com/sharer.php?u=' . $route, '', $code) . '</li>';
-		$output .= '<li>' . anchor_element('external', '', 'link_share_this link_google', 'Google', 'plusone.google.com/_/+1/confirm?url=' . $route, '', $code) . '</li>';
-		$output .= '<li>' . anchor_element('external', '', 'link_share_this link_twitter', 'Twitter', 'twitter.com/share?url=' . $route, '', $code) . '</li>';
-		$output .= '<li>' . anchor_element('external', '', 'link_share_this link_pinterest', 'Pinterest', 'pinterest.com/pin/create/button/?url=' . $route, '', $code) . '</li>';
-		$output .= '<li>' . anchor_element('external', '', 'link_share_this link_tumblr', 'Tumblr', 'tumblr.com/share', '', $code) . '</li>';
-		$output .= '<li>' . anchor_element('external', '', 'link_share_this link_linkedin', 'Linkedin', 'linkedin.com/shareArticle?url=' . $route, '', $code) . '</li>';
-		$output .= '<li>' . anchor_element('external', '', 'link_share_this link_stumbleupon', 'Stumbleupon', 'stumbleupon.com/submit?url=' . $route, '', $code) . '</li>';
-		$output .= '<li>' . anchor_element('external', '', 'link_share_this link_delicious', 'Delicious', 'del.icio.us/post?url=' . $route, '', $code) . '</li>';
+		$output .= '<li>' . anchor_element('external', '', 'js_link_share_this link_share_this link_facebook', 'Facebook', 'facebook.com/sharer.php?u=' . $route, '', $code . ' data-height="280"') . '</li>';
+		$output .= '<li>' . anchor_element('external', '', 'js_link_share_this link_share_this link_google', 'Google', 'plusone.google.com/_/+1/confirm?url=' . $route, '', $code . ' data-height="450"') . '</li>';
+		$output .= '<li>' . anchor_element('external', '', 'js_link_share_this link_share_this link_twitter', 'Twitter', 'twitter.com/share?url=' . $route, '', $code . ' data-height="460"') . '</li>';
+		$output .= '<li>' . anchor_element('external', '', 'js_link_share_this link_share_this link_pinterest', 'Pinterest', 'pinterest.com/pin/create/button/?url=' . $route, '', $code . ' data-height="500" data-width="800"') . '</li>';
+		$output .= '<li>' . anchor_element('external', '', 'js_link_share_this link_share_this link_tumblr', 'Tumblr', 'tumblr.com/share', '', $code) . '</li>';
+		$output .= '<li>' . anchor_element('external', '', 'js_link_share_this link_share_this link_linkedin', 'Linkedin', 'linkedin.com/shareArticle?url=' . $route, '', $code . ' data-height="450" data-width="850"') . '</li>';
+		$output .= '<li>' . anchor_element('external', '', 'js_link_share_this link_share_this link_stumbleupon', 'Stumbleupon', 'stumbleupon.com/submit?url=' . $route, '', $code . ' data-width="850"') . '</li>';
+		$output .= '<li>' . anchor_element('external', '', 'js_link_share_this link_share_this link_delicious', 'Delicious', 'del.icio.us/post?url=' . $route, '', $code . ' data-height="380"') . '</li>';
 		$output .= '</ul></div>';
 		return $output;
 	}
