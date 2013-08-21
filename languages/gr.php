75641b4415e7caf2a5e7d0fb6248fd83e03fd5fa
redaxmedia <info@redaxmedia.com>
2013-08-21 10:40:10 +0200

    Create gr.php

---
+++ languages/gr.php
@@ -0,0 +1,350 @@
+<?php
+
+/* site */
+
+$l['home'] = 'Αρχικη σελιδα';
+$l['administration'] = 'Διαχειρηση';
+$l['system'] = 'Συστημα';
+$l['error'] = 'Ερρορ';
+
+/* contents */
+
+$l['content'] = 'Περιεχομενο';
+$l['contents'] = 'Περιεχομενα';
+$l['content_not_found'] = 'Το περιεχομενο δεν βρεθηκε';
+
+/* categories */
+
+$l['category'] = 'Κατηγορία';
+$l['categories'] = 'Κατηγοριες';
+$l['category_new'] = 'Νεα κατηγορια';
+$l['category_no'] = 'Δεν υπαρχει κατηγορια';
+$l['category_parent'] = 'Μητρικη Κατηγορια';
+$l['uncategorized'] = 'Χωρίς κατηγορία';
+
+/* articles */
+
+$l['article'] = 'Άρθρο';
+$l['articles'] = 'Αρθρα';
+$l['article_new'] = 'Νεο Αρθρο';
+$l['article_no'] = 'Δεν υπαρχει αρθρο';
+
+/* extras */
+
+$l['extra'] = 'Πρόσθετο';
+$l['extras'] = 'Προσθετα';
+$l['extra_new'] = 'Νεα προσθετα';
+$l['extra_no'] = 'Κανένα πρόσθετο';
+
+/* comments */
+
+$l['comment'] = 'Σχόλιο';
+$l['comments'] = 'Σχολια';
+$l['comment_new'] = 'Νέο σχόλιο';
+$l['comment_no'] = 'Κανένα σχόλιο';
+$l['comment_empty'] = 'Δεν υπάρχουν σχόλια';
+$l['comment_sent'] = 'Το σχόλιο σου στάλθηκε με επιτυχία';
+$l['comment_moderation'] = 'Το σχόλιο σου θα πρέπει πρώτα να επιβεβαιωθεί απο τον διαχειριστή';
+
+/* search */
+
+$l['search'] = 'Αναζήτηση';
+$l['search_terms'] = 'Όροι αναζήτησης';
+$l['search_no'] = 'Δεν βρέθηκαν αποτελέσματα στην αναζήτηση που κάνατε';
+
+/* time */
+
+$l['time'] = 'Ώρα';
+$l['date'] = 'Ημερομηνια';
+$l['minute'] = 'Λεπτό';
+$l['hour'] = 'Ώρα';
+$l['day'] = 'Ημέρα';
+$l['month'] = 'Μήνας';
+$l['year'] = 'Χρόνος';
+$l['month_names'] = 'Ιανουάριος, Φεβρουάριος, Μάρτιος, Απρίλιος, Μάιος, Ιούνιος, Ιούλιος, Αύγουστος, Σεπτέμβριος, Οκτώβριος, Νοέμβριος, Δεκέμβριος';
+$l['today'] = 'Σήμερα';
+$l['yesterday'] = 'Χθές';
+$l['on'] = 'Τον';
+$l['at'] = 'στις';
+
+/* numbers */
+
+$l['1'] = 'Ένα';
+$l['2'] = 'Δύο';
+$l['3'] = 'Τρία';
+$l['4'] = 'Τέσσερα';
+$l['5'] = 'Πέντε';
+$l['6'] = 'Έξι';
+$l['7'] = 'Εφτά';
+$l['8'] = 'Οχτώ';
+$l['9'] = 'Εννιά';
+$l['10'] = 'Δέκα';
+
+/* math */
+
+$l['plus'] = 'συν';
+$l['minus'] = 'πλην';
+$l['random'] = 'τυχαιο';
+$l['addition'] = 'πρόσθεση';
+$l['subtraction'] = 'αφαίρεση';
+
+/* navigation */
+
+$l['first'] = 'Πρώτος';
+$l['previous'] = 'Προηγούμενος';
+$l['next'] = 'Επόμενος';
+$l['last'] = 'Τελευταίος';
+$l['continue'] = 'Συνέχεια';
+$l['back'] = 'Πίσω';
+$l['up'] = 'Πάνω';
+$l['down'] = 'Κάτω';
+$l['sort'] = 'Ειδος';
+$l['new'] = 'Νέο';
+$l['view'] = 'View';
+$l['read_more'] = 'Διαβάστε περισσότερα';
+
+/* buttons */
+
+$l['create'] = 'Δημιουργια';
+$l['edit'] = 'Επεξεργασια';
+$l['delete'] = 'Διαγραφη';
+$l['save'] = 'Αποθηκευση';
+$l['reset'] = 'Επαναφορά';
+$l['submit'] = 'Υποβολή';
+$l['ok'] = 'Εντάξει';
+$l['cancel'] = 'Ακυρωση';
+
+/* status */
+
+$l['future_posting'] = 'Future posting';
+
+/* actions */
+
+$l['publish'] = 'Δημοσίευση';
+$l['unpublish'] = 'Καταργηση δημοσιευσης';
+$l['enable'] = 'Ενεργοποίηση';
+$l['disable'] = 'Απενεργοποίηση';
+$l['freeze'] = 'Πάγωμα';
+$l['restrict'] = 'Περιορισμός';
+$l['install'] = 'Εγκατασταση';
+$l['uninstall'] = 'Απεγκατάσταση';
+$l['restore'] = 'Επαναφορά';
+$l['process'] = 'Διαδικασία';
+$l['update'] = 'Ενημέρωση';
+$l['select'] = 'Επιλέξτε';
+
+/* form */
+
+$l['title'] = 'Τιτλος';
+$l['title_exists'] = 'Ο Τίτλος υπάρχει ήδη';
+$l['title_incorrect'] = 'Ο Τίτλος είναι εσφαλμένος';
+$l['title_empty'] = 'Δεν υπάρχει τίτλος';
+$l['alias'] = 'Ψευδωνυμο';
+$l['alias_exists'] = 'Το ψευδώνυμο ​​υπάρχει ήδη';
+$l['alias_incorrect'] = 'Το ψευδώνυμο είναι εσφαλμένο';
+$l['alias_empty'] = 'Δεν υπάρχει ψευδώνυμο';
+$l['name'] = 'Όνομα';
+$l['name_incorrect'] = 'Το όνομα είναι εσφαλμένο';
+$l['name_empty'] = 'Δεν υπάρχει όνομα';
+$l['author'] = 'Συγγραφέας';
+$l['author_incorrect'] = 'Ο Συγγραφέας είναι εσφαλμένος';
+$l['author_empty'] = 'Δεν υπάρχει συγγραφέας';
+$l['text'] = 'Κείμενο';
+$l['text_empty'] = 'Το κείμενο δεν υπάρχει ';
+$l['message'] = 'Μήνυμα';
+$l['message_empty'] = 'Δεν υπάρχει μήνυμα';
+$l['customize'] = 'Προσαρμογη';
+$l['filter'] = 'Φιλτρο';
+$l['status'] = 'Κατάσταση';
+$l['rank'] = 'Βαθμίδα';
+$l['headline'] = 'Επικεφαλίδα';
+$l['infoline'] = 'Γραμμή πληροφοριών';
+
+/* input */
+
+$l['input'] = 'Input';
+$l['inputs'] = 'Inputs';
+$l['input_incorrect'] = 'Input is incorrect';
+$l['input_empty'] = 'Input is empty';
+
+/* select */
+
+$l['none'] = 'None';
+$l['all'] = 'All';
+
+/* legend */
+
+$l['fields_request'] = 'Εισάγετε τις αντίστοιχες πληροφορίες στα ακόλουθα πεδία';
+$l['fields_required'] = 'Τα υποχρεωτικά πεδία σημειώνονται με αστερίσκο';
+$l['fields_limited'] = 'Τα ακόλουθα πεδία περιορίζονται σε 5-10 χαρακτήρες';
+
+/* email */
+
+$l['email'] = 'E-mail';
+$l['email_incorrect'] = 'E-mail είναι εσφαλμένο';
+$l['email_empty'] = 'Δεν υπάρχουν E-mails';
+$l['email_unknown'] = 'E-mail είναι άγνωστο';
+
+/* computing */
+
+$l['url'] = 'URL';
+$l['url_incorrect'] = 'URL είναι εσφαλμένο';
+$l['identifier'] = 'Αναγνωριστικο';
+$l['token'] = 'Token';
+$l['token_incorrect'] = 'Token είναι εσφαλμένο';
+
+/* installation */
+
+$l['installation'] = 'Εγκατάσταση';
+$l['installation_completed'] = 'Η Εγκατάσταση ολοκληρώθηκε με επιτυχία';
+
+/* database */
+
+$l['host'] = 'Host';
+$l['prefix'] = 'Prefix';
+$l['database'] = 'βάση δεδομένων';
+$l['database_setup'] = 'Ρύθμιση της βάσης δεδομένων σας';
+$l['database_failed'] = 'Η Σύνδεση με τη βάση δεδομένων απέτυχε';
+
+/* registration */
+
+$l['registration'] = 'Εγγραφή';
+$l['registration_sent'] = 'Οι Πληροφορίες της εγγραφής σας έχουν σταλεί με επιτυχία';
+$l['registration_verification'] = 'Η εγγραφή σας θα πρέπει να εγκριθεί από το διαχειριστή';
+$l['account_create'] = 'Δημιουργήστε το λογαριασμό σας';
+
+/* reminder */
+
+$l['reminder'] = 'Υπενθύμιση';
+$l['reminder_question'] = 'Μήπως ξεχάσατε τον χρήστη ή τον κωδικό πρόσβασης σας';
+$l['reminder_request'] = 'Πληκτρολογήστε το e-mail σας για να λάβετε τις αντίστοιχες πληροφορίες του λογαριασμού σας';
+$l['reminder_sent'] = 'Τα στοιχεία του λογαριασμού σας έχουν σταλεί με επιτυχία';
+
+/* login */
+
+$l['login'] = 'Σύνδεση';
+$l['login_incorrect'] = 'Η σύνδεση  είναι εσφαλμένη';
+$l['logged_in'] = 'Είστε συνδεδεμένος στο σύστημα';
+$l['logged_out'] = 'Έχεται αποσυνδεθει απο το σύστημα';
+$l['logout'] = 'Αποσυνδεση';
+
+/* salutation */
+
+$l['welcome'] = 'Καλώς ορίσατε';
+$l['hello'] = 'Χαίρετε';
+$l['goodbye'] = 'Αντίο';
+
+/* password */
+
+$l['password'] = 'Κωδικός';
+$l['password_new'] = 'Νέος κωδικός';
+$l['password_confirm'] = 'Επιβεβαιώστε τον κωδικό πρόσβασης';
+$l['password_incorrect'] = 'Ο Κωδικος πρόσβασης είναι εσφαλμένος';
+$l['password_empty'] = 'Δεν υπάρχει κωδικός';
+$l['password_reset'] = 'Επαναφοράς κωδικού πρόσβασης';
+$l['password_sent'] = 'Ο Κωδικός πρόσβασης έχει αποσταλεί με επιτυχία';
+
+/* groups */
+
+$l['group'] = 'Ομάδα';
+$l['groups'] = 'Ομαδες';
+$l['group_new'] = 'Νεα ομαδα';
+$l['group_no'] = 'Δεν υπάρχει ομάδα';
+
+/* users */
+
+$l['user'] = 'Χρηστης';
+$l['users'] = 'χρηστες';
+$l['user_new'] = 'Νέος χρήστης';
+$l['user_no'] = 'Δεν υπάρχουν χρήστες';
+$l['user_exists'] = 'Ο Χρήστης υπάρχει ήδη';
+$l['user_incorrect'] = 'Ο χρήστης είναι εσφαλμένος';
+$l['user_empty'] = 'Δεν υπάρχει χρήστης';
+$l['session'] = 'Session';
+$l['online'] = 'Συνδεδεμένοι';
+$l['posted_by'] = 'Δημοσιεύτηκε από';
+$l['profile'] = 'προφιλ';
+
+/* access */
+
+$l['access'] = 'προσβαση';
+$l['access_no'] = 'Δεν επιτρέπεται η πρόσβαση';
+
+/* modules */
+
+$l['module'] = 'Module';
+$l['modules'] = 'Modules';
+$l['module_no'] = 'No module present';
+$l['version'] = 'Εκδοση';
+
+/* settings */
+
+$l['settings'] = 'Ρυθμισεις';
+$l['general'] = 'Γενικα';
+$l['language'] = 'Γλώσσα';
+$l['languages'] = 'Γλώσσες';
+$l['detect'] = 'Εντοπισμός';
+$l['template'] = 'Template';
+$l['templates'] = 'Templates';
+$l['pagination'] = 'Σελιδοποίηση';
+$l['metadata'] = 'Μεταδεδομενα';
+$l['copyright'] = 'Πνευματική ιδιοκτησία';
+$l['description'] = 'Περιγραφή';
+$l['keywords'] = 'Λέξεις-κλειδιά';
+$l['robots'] = 'Ρομπότς';
+$l['index'] = 'Ευρετήριο';
+$l['index_no'] = 'No Index';
+$l['formatting'] = 'Μορφοποιηση';
+$l['charset'] = 'Σύνολο χαρακτήρων';
+$l['divider'] = 'Divider';
+$l['homepage'] = 'Αρχικη σελιδα';
+$l['limit'] = 'Οριο';
+$l['order'] = 'Παραγγελία';
+$l['ascending'] = 'Αύξουσα';
+$l['descending'] = 'Φθίνουσα';
+$l['contact'] = 'Επαφη';
+$l['subject'] = 'Θέμα';
+$l['notification'] = 'Ειδοποίηση';
+$l['moderation'] = 'Moderation';
+$l['verification'] = 'Επιβεβαίωση';
+
+/* security */
+
+$l['security'] = 'Ασφάαλεια';
+$l['captcha'] = 'Captcha';
+$l['captcha_incorrect'] = 'Το Captcha είναι εσφαλμένο';
+$l['blocker'] = 'Blocker';
+
+/* notification */
+
+$l['operation_completed'] = 'Η λειτουργία ολοκληρώθηκε με επιτυχία';
+$l['error_occurred'] = 'Παρουσιάστηκε σφάλμα';
+$l['something_wrong'] = 'Κάτι πήγε στραβά';
+
+/* dialog */
+
+$l['dialog'] = 'Dialog';
+$l['dialog_question'] = 'Θέλετε να συνεχίσετε';
+$l['alert'] = 'Ειδοποίηση';
+$l['confirm'] = 'Επιβεβαιώση';
+$l['prompt'] = 'Προτροπή';
+
+/* files */
+
+$l['file_remove'] = 'Αφαιρέστε το αρχείο';
+$l['file_permission_grant'] = 'Χορήγηση άδειας εγγραφής στο αρχείο';
+$l['file_permission_revoke'] = 'Ανάκληση άδειας εγγραφής στο αρχείο';
+$l['file_not_found'] = 'Το Αρχείο δεν βρέθηκε';
+
+/* about */
+
+$l['design_and_realization'] = 'Σχεδιασμός και υλοποίηση από';
+$l['powered_by'] = 'Με την δύναμη του';
+
+/* characters */
+
+$l['point'] = '.';
+$l['colon'] = ':';
+$l['question_mark'] = '?';
+$l['exclamation_mark'] = '!';
+?>
