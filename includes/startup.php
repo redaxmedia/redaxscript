bb149d7ebf3534ad684d6f6fb816c7220991cb9e
redaxmedia <info@redaxmedia.com>
2013-08-30 11:18:13 +0200

    Another hotfix for last id and last table

--- includes/startup.php
+++ includes/startup.php
@@ -115,7 +115,7 @@ function startup()
 
 	/* define tables */
 
-	if (FULL_ROUTE == '' && check_alias(FIRST_PARAMETER, 1) == 0)
+	if (FULL_ROUTE == '' || (FIRST_PARAMETER == 'admin' && SECOND_PARAMETER == ''))
 	{
 		/* check for homepage */
 
