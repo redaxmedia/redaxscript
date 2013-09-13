f9edcd940454b6dcaf14f244b94fd498282d9725
kimkha <nkimkha@gmail.com>
2013-09-13 20:56:15 +0700

    Add vi language for QUnit module

---
+++ modules/qunit/languages/vi.php
@@ -0,0 +1,12 @@
+<?php
+
+/* qunit */
+
+$l['qunit_title'] = 'QUnit';
+$l['qunit_description'] = 'Kiểm thử từng phần Javascript';
+$l['qunit_type_expected'] = 'Kiểu mong đợi';
+$l['qunit_value_expected'] = 'Giá trị mong đợi';
+$l['qunit_attribute_expected'] = 'Thuộc tính mong đợi';
+$l['qunit_test_passed'] = 'Kiểm tra thành công';
+$l['qunit_test_failed'] = 'Kiểm tra thất bại';
+?>
