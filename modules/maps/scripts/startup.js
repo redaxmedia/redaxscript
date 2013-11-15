f6c9b31212237b39f5c36bb9a992101112edc034
redaxmedia <info@redaxmedia.com>
2013-11-15 22:42:47 +0100

    Maps cosmetics

--- modules/maps/scripts/startup.js
+++ modules/maps/scripts/startup.js
@@ -29,9 +29,9 @@ r.modules.maps =
 			center: new google.maps.LatLng(0, 0),
 			mapTypeId: google.maps.MapTypeId.ROADMAP,
 			mapTypeControl: false,
-			panControl: true,
 			scaleControl: false,
 			streetViewControl: false,
+			panControl: true,
 			overviewMapControl: false,
 			zoomControl: true
 		},
@@ -85,7 +85,7 @@ r.modules.maps =
 			}
 		],
 		marker: true,
-		deBranding: true,
+		deBrand: true,
 		mapLogo: '<a href="http://maps.google.com" target="_blank">Google Maps</a>',
 		mapTerms: '<a href="http://google.com/intl/en/help/terms_maps.html" target="_blank">Terms of use</a>'
 	}
