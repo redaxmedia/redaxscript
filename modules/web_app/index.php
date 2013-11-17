003e0606ae743890617937e0180922988e381a9b
redaxmedia <info@redaxmedia.com>
2013-11-17 23:08:36 +0100

    Switch to old icon

--- modules/web_app/index.php
+++ modules/web_app/index.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * web app loader start
  *
@@ -16,7 +17,6 @@ function web_app_loader_start()
 	$loader_modules_scripts[] = 'modules/web_app/scripts/startup.js';
 	$loader_modules_scripts[] = 'modules/web_app/scripts/web_app.js';
 }
-
 
 /**
  * web app render start
@@ -37,5 +37,40 @@ function web_app_render_start()
 		include_once('modules/web_app/files/manifest.json');
 		define('RENDER_BREAK', 1);
 	}
+}
+
+/**
+ * web app head start
+ *
+ * @since 2.0.2
+ * @deprecated 2.0.0
+ *
+ * @package Redaxscript
+ * @category Modules
+ * @author Henry Ruhs
+ */
+
+function web_app_head_start()
+{
+	$output = '<meta name="mobile-web-app-capable" content="yes">' . PHP_EOL;
+	$output .= '<meta name="apple-mobile-web-app-capable" content="yes">' . PHP_EOL;
+	echo $output;
+}
+
+/**
+ * web app head end
+ *
+ * @since 2.0.2
+ * @deprecated 2.0.0
+ *
+ * @package Redaxscript
+ * @category Modules
+ * @author Henry Ruhs
+ */
+
+function web_app_head_end()
+{
+	$output = '<link href="' . ROOT . '/modules/web_app/images/icon.png" rel="apple-touch-icon-precomposed" />' . PHP_EOL;
+	echo $output;
 }
 ?>
