03da2f5108a5fc3ab752d33627a13facd1e8c182
redaxmedia <info@redaxmedia.com>
2013-12-09 21:42:22 +0100

    Switch to global cache folder

--- modules/fb_group/config.php
+++ modules/fb_group/config.php
@@ -8,7 +8,7 @@ define(FB_GROUP_GROUP_URL, '');
 define(FB_GROUP_APP_ID, '');
 define(FB_GROUP_APP_SECRET, '');
 define(FB_GROUP_GROUP_ID, '');
-define(FB_GROUP_CACHE_PATH, 'modules/fb_group/cache');
+define(FB_GROUP_CACHE_PATH, 'cache/fb_group');
 define(FB_GROUP_CACHE_FEED_EXPIRES, 1800);
 define(FB_GROUP_CACHE_MEMBERS_EXPIRES, 3600);
 ?>
