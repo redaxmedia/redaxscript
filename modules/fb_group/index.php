03cd7ffc875257ea704a918e8112d3cc227cd820
redaxmedia <info@redaxmedia.com>
2013-12-15 20:32:21 +0100

    Cosmetics on fb group module

--- modules/fb_group/index.php
+++ modules/fb_group/index.php
@@ -406,6 +406,7 @@ function fb_group_get_contents($type = '')
 			$url = FB_GROUP_API_URL . '/' . FB_GROUP_GROUP_ID . '/' . $type. '/?access_token=' . $access_token;
 			$ch = curl_init();
 			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 			curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 10);
 			$output = curl_exec($ch);
 			curl_close($ch);
