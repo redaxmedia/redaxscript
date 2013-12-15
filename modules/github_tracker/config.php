ef430030a7318bd8a5599b5107b5dd6bc03deaa7
redaxmedia <info@redaxmedia.com>
2013-12-15 22:18:08 +0100

    Remove config from module

--- modules/github_tracker/config.php
+++ modules/github_tracker/config.php
@@ -3,8 +3,8 @@
 /* config github tracker */
 
 define(GITHUB_TRACKER_API_URL, 'https://api.github.com/repos');
-define(GITHUB_TRACKER_USER, 'redaxmedia');
-define(GITHUB_TRACKER_REPO, 'redaxscript');
+define(GITHUB_TRACKER_USER, '');
+define(GITHUB_TRACKER_REPO, '');
 define(GITHUB_TRACKER_CACHE_PATH, 'cache/github_tracker');
 define(GITHUB_TRACKER_CACHE_ISSUES_EXPIRES, 1800);
 define(GITHUB_TRACKER_CACHE_MILESTONES_EXPIRES, 3600);
