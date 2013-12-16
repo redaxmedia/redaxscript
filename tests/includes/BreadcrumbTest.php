183f7d1be0e59db2ccda5ad5fba2460cebb1eede
redaxmedia <info@redaxmedia.com>
2013-12-16 18:38:55 +0100

    Breadcrumb dummy test

---
+++ tests/includes/BreadcrumbTest.php
@@ -0,0 +1,20 @@
+<?php
+
+/**
+ * breadcrumb test
+ *
+ * @since 2.1.0
+ *
+ * @package Redaxscript
+ * @category Tests
+ * @author Henry Ruhs
+ */
+
+class Redaxscript_Breadcrumb_Test extends PHPUnit_Framework_TestCase
+{
+	public function testDummy()
+	{
+		$this->assertTrue(true);
+	}
+}
+?>
