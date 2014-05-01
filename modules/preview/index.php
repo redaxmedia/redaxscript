c873134ec378e57ec84b1b1ba032a04a414a9bfd
redaxmedia <info@redaxmedia.com>
2014-05-01 22:39:29 +0200

    Preview module splitted into partials

--- modules/preview/index.php
+++ modules/preview/index.php
@@ -80,13 +80,18 @@ function preview_center_start()
 			include_once('modules/preview/partials/typography.phtml');
 			include_once('modules/preview/partials/box.phtml');
 			include_once('modules/preview/partials/form.phtml');
+			include_once('modules/preview/partials/form_admin.phtml');
 			include_once('modules/preview/partials/icon.phtml');
 			include_once('modules/preview/partials/media.phtml');
 			include_once('modules/preview/partials/interface.phtml');
 			include_once('modules/preview/partials/accordion.phtml');
+			include_once('modules/preview/partials/accordion_admin.phtml');
 			include_once('modules/preview/partials/tab.phtml');
+			include_once('modules/preview/partials/tab_admin.phtml');
 			include_once('modules/preview/partials/table.phtml');
+			include_once('modules/preview/partials/table_admin.phtml');
 			include_once('modules/preview/partials/dialog.phtml');
+			include_once('modules/preview/partials/dialog_admin.phtml');
 			include_once('modules/preview/partials/note.phtml');
 		}
 		$output .= ob_get_clean() . PHP_EOL;
