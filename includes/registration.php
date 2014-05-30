c593a3c7a270c1c47c20772424589c22671a08f1
redaxmedia <info@redaxmedia.com>
2014-05-30 04:34:57 +0200

    Update registration.php

--- includes/registration.php
+++ includes/registration.php
@@ -131,10 +131,6 @@ function registration_post()
 	{
 		$error = l('user_exists');
 	}
-	else if (retrieve('id', 'users', 'email', $email))
-	{
-		$error = l('user_exists');
-	}
 	else
 	{
 		if (USERS_NEW == 0 && s('verification') == 1)
