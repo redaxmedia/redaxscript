a121a88320ac329a6d6576c850f8eb14a209a3dd
Sven Weingartner <git@communitales.com>
2014-06-10 23:10:27 +0200

    Test for autoloader. First classes for #116

---
+++ includes/Validator/Interface.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * interface for a validator
+ *
+ * @since 2.2.0
+ *
+ * @package Redaxscript
+ * @category Validator
+ * @author Sven Weingartner
+ */
+
+interface Redaxscript_Validator_Interface {
+
+	/**
+	 * checkes the validator
+	 *
+	 * @since 2.2.0
+	 *
+	 * @return bool
+	 */
+
+	public function validate();
+}
+ 
--- tests/includes/autoloader_test.php
+++ tests/includes/autoloader_test.php
@@ -8,10 +8,26 @@
  * @package Redaxscript
  * @category Tests
  * @author Henry Ruhs
+ * @author Sven Weingartner
  */
-
 class Redaxscript_Autoloader_Test extends PHPUnit_Framework_TestCase
 {
+
+	/**
+	 * providerFilePath
+	 *
+	 * @since 2.1.0
+	 *
+	 * @return array
+	 */
+
+	public function providerFilePath()
+	{
+		$contents = file_get_contents('tests/provider/autoloader_file_path.json');
+		$output = json_decode($contents, true);
+		return $output;
+	}
+
 	/**
 	 * testDirectory
 	 *
@@ -32,6 +48,28 @@ class Redaxscript_Autoloader_Test extends PHPUnit_Framework_TestCase
 		/* compare */
 
 		$this->assertEquals('includes', $result);
+	}
+
+	/**
+	 * testAutoloadFilePath
+	 *
+	 * @since 2.2.0
+	 *
+	 * @param string $className
+	 * @param string $expect
+	 *
+	 * @dataProvider providerFilePath
+	 */
+
+	public function testAutoloadFilePath($className = '', $expect = '')
+	{
+		/* result */
+
+		$result = class_exists($className);
+
+		/* compare */
+
+		$this->assertEquals($expect, $result, 'Classname: ' . $className);
 	}
 
 	/**
