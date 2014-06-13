a98a6ed52186a7e924d2979ca870fdf69e5bf5d9
redaxmedia <info@redaxmedia.com>
2014-06-13 21:52:40 +0200

    Introducing dummy Language class

--- includes/Language.php
+++ includes/Language.php
@@ -1,7 +1,7 @@
 <?php
 
 /**
- * parent class to provide needed language
+ * parent class to provide language
  *
  * @since 2.2.0
  *
@@ -12,5 +12,64 @@
 
 class Redaxscript_Language
 {
+	/**
+	 * instance of the registry class
+	 *
+	 * @var object
+	 */
 
+	protected $_registry;
+
+	/**
+	 * constructor of the class
+	 *
+	 * @since 2.2.0
+	 *
+	 * @param Redaxscript_Registry $registry instance of the registry class
+	 */
+
+	public function __construct(Redaxscript_Registry $registry)
+	{
+		$this->_registry = $registry;
+		$this->init();
+	}
+
+	/**
+	 * init the class
+	 *
+	 * @since 2.2.0
+	 */
+
+	public function init($name = null)
+	{
+		/* merge things one time */
+		global $l;
+		$output = '';
+		if ($l === null)
+		{
+			$l = array();
+		}
+		$registry = Redaxscript_Registry::instance();
+		$language = $registry->get('language');
+		$language = 'en';
+		$json_default = json_decode(file_get_contents('languages/' . $language . '.json'), true);
+		$json_current = array();
+		if ($language !== 'en')
+		{
+			$json_current = json_decode(file_get_contents('languages/' . $language . '.json'), true);;
+		}
+		if (is_array($json_default))
+		{
+			$l = array_merge($l, $json_default, $json_current);
+		}
+		if (array_key_exists($name, $l))
+		{
+			$output = $l[$name];
+			if (function_exists('mb_convert_encoding'))
+			{
+				$output = mb_convert_encoding($l[$name], s('charset'), 'utf-8, latin1');
+			}
+		}
+		return $output;
+	}
 }
--- includes/migrate.php
+++ includes/migrate.php
@@ -105,37 +105,15 @@ function migrate_constants()
  * @category Migrate
  * @author Henry Ruhs
  *
+ * @param string $name
+ *
  * @return string
  */
 
 function l($name = null)
 {
-	global $l;
-	$output = '';
-	if ($l === null)
-	{
-		$l = array();
-	}
 	$registry = Redaxscript_Registry::instance();
-	$language = $registry->get('language');
-	$language = 'en';
-	$json_default = json_decode(file_get_contents('languages/' . $language . '.json'), true);
-	$json_current = array();
-	if ($language !== 'en')
-	{
-		$json_current = json_decode(file_get_contents('languages/' . $language . '.json'), true);;
-	}
-	if (is_array($json_default))
-	{
-		$l = array_merge($l, $json_default, $json_current);
-	}
-	if (array_key_exists($name, $l))
-	{
-		$output = $l[$name];
-		if (function_exists('mb_convert_encoding'))
-		{
-			$output = mb_convert_encoding($l[$name], s('charset'), 'utf-8, latin1');
-		}
-	}
+	$language = new Redaxscript_Language($registry);
+	$output = $language->init($name);
 	return $output;
 }
