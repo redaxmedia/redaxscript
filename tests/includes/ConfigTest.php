5f342e22edcb0e12610b4a54f8e808ccdfc943f9
redaxmedia <info@redaxmedia.com>
2014-06-18 18:12:46 +0200

    Create ConfigTest.php

---
+++ tests/includes/ConfigTest.php
@@ -0,0 +1,25 @@
+<?php
+
+/**
+ * Redaxscript Mailer Test
+ *
+ * @since 2.2.0
+ *
+ * @package Redaxscript
+ * @category Tests
+ * @author Henry Ruhs
+ */
+
+class Redaxscript_Config_Test extends PHPUnit_Framework_TestCase
+{
+	/**
+	 * testDummy
+	 *
+	 * @since 2.20
+	 */
+
+	public function testDummy()
+	{
+		$this->assertTrue(true);
+	}
+}
