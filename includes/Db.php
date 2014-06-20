0af9626c1f82aa64e2263a6f18b03defd6d098ed
redaxmedia <info@redaxmedia.com>
2014-06-20 10:51:41 +0200

    Update Db.php

--- includes/Db.php
+++ includes/Db.php
@@ -12,15 +12,23 @@
 
 class Redaxscript_Db extends ORM
 {
+        /**
+	 * connect to database
+	 *
+	 * @since 2.2.0
+	 *
+	 * @param string $type database type
+	 */
+
 	function static connect($type = 'mysql')
 	{
 		/* handle mysql */
 
 		if ($type === 'mysql')
 		{
-			self::configure('mysql:host=' . $host . ';dbname=' . $name);
-			self::configure('username', $user);
-			self::configure('password', $password);
+			self::configure('mysql:host=' . Redaxscript_Config::get('host') . ';dbname=' . Redaxscript_Config::get('name'));
+			self::configure('username', Redaxscript_Config::get('user'));
+			self::configure('password', Redaxscript_Config::get('password'));
 			self::configure('driver_options', array(
 				PDO::MYSQL_ATTR_INIT_COMMAND => 'SET NAMES utf8'
 			));
@@ -28,8 +36,9 @@ class Redaxscript_Db extends ORM
 		
 		/* register database */
 		
-		//$_SESSION[ROOT . '/db_connected'] = 1;
-		//$_SESSION[ROOT . '/db_error'] = '';
+		$registry = Redaxscript_Registry::getInstance();
+		$registry->set('dbConnected', true);
+		$registry->set('dbError', false);
 	}
 
         /**
