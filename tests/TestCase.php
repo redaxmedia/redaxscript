d1de0d4721990a86feb7eeab762fdcec498ca950
redaxmedia <info@redaxmedia.com>
2014-08-24 21:18:35 +0200

    Improve TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -21,14 +21,15 @@ class TestCase extends PHPUnit_Framework_TestCase
 	 * @since 2.2.0
 	 *
 	 * @param string $json
+	 * @param boolean $assoc
 	 *
 	 * @return array
 	 */
 
-	public function getProvider($json = null)
+	public function getProvider($json = null, $assoc = true)
 	{
 		$contents = file_get_contents($json);
-		$output = json_decode($contents, true);
+		$output = json_decode($contents, $assoc);
 		return $output;
 	}
 }
