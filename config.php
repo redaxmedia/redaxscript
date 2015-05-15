424103d9b8e2e75e5db1c84f36c1eee622afab35
redaxmedia <info@redaxmedia.com>
2015-05-15 22:56:53 +0200

    Fresh heroku branch

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
+$dbUrl = parse_url(getenv('DATABASE_URL'));
 return array(
-	'dbType' => '',
-	'dbHost' => '',
-	'dbName' => '',
-	'dbUser' => '',
-	'dbPassword' => '',
-	'dbPrefix' => '',
+	'dbType' => 'pgsql',
+	'dbHost' => $dbUrl['host'],
+	'dbName' => trim($dbUrl['path'], '/'),
+	'dbUser' => $dbUrl['user'],
+	'dbPassword' => $dbUrl['pass'],
+	'dbPrefix' => 'rxs_',
 	'dbSalt' => ''
 );
