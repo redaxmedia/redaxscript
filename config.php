7213c0f4d42c0abdeda3a549285d026a3fd208c4
redaxmedia <info@redaxmedia.com>
2015-05-16 22:18:53 +0200

    Remove the db prefix

--- config.php
+++ config.php
@@ -6,6 +6,6 @@ return array(
 	'dbName' => trim($dbUrl['path'], '/'),
 	'dbUser' => $dbUrl['user'],
 	'dbPassword' => $dbUrl['pass'],
-	'dbPrefix' => '',
+	'dbPrefix' => 'rs_',
 	'dbSalt' => 'b66e42b397a215cec2ecb3d7a5bdae1b6738abc9'
 );
