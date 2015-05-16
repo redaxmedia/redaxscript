b8e2236d389ac1499babbe8a6412e918cbded90a
redaxmedia <info@redaxmedia.com>
2015-05-16 22:03:24 +0200

    Remove the db prefix

--- config.php
+++ config.php
@@ -1,10 +1,11 @@
 <?php
+$dbUrl = parse_url(getenv('DATABASE_URL'));
 return array(
-	'dbType' => '',
-	'dbHost' => '',
-	'dbName' => '',
-	'dbUser' => '',
-	'dbPassword' => '',
+	'dbType' => 'pgsql',
+	'dbHost' => $dbUrl['host'],
+	'dbName' => trim($dbUrl['path'], '/'),
+	'dbUser' => $dbUrl['user'],
+	'dbPassword' => $dbUrl['pass'],
 	'dbPrefix' => '',
-	'dbSalt' => ''
+	'dbSalt' => 'b66e42b397a215cec2ecb3d7a5bdae1b6738abc9'
 );
