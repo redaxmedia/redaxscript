2e66472cdb5e863fd23cad663a13fd24234564f7
redaxmedia <info@redaxmedia.com>
2016-01-06 22:51:17 +0100

    Fix docblocks and introduce admin forms

--- includes/Admin/View/ArticleForm.php
+++ includes/Admin/View/ArticleForm.php
@@ -1,2 +1,55 @@
 <?php
 namespace Redaxscript\Admin\View;
+
+use Redaxscript\Admin\Html\Form as AdminForm;
+use Redaxscript\Html;
+use Redaxscript\Hook;
+use Redaxscript\Language;
+use Redaxscript\Registry;
+
+/**
+ * children class to render the article form
+ *
+ * @since 3.0.0
+ *
+ * @package Redaxscript
+ * @category Admin
+ * @author Henry Ruhs
+ */
+
+class ArticleForm implements ViewInterface
+{
+	/**
+	 * render the view
+	 *
+	 * @since 3.0.0
+	 *
+	 * @return string
+	 */
+
+	public function render()
+	{
+		$output = Hook::trigger('adminArticleFormStart');
+
+		/* html elements */
+
+		$titleElement = new Html\Element();
+		$titleElement->init('h2', array(
+			'class' => 'rs-admin-title-content',
+		));
+		$titleElement->text(Language::get('article'));
+		$formElement = new AdminForm(Registry::getInstance(), Language::getInstance());
+		$formElement->init();
+
+		/* create the form */
+
+		$formElement
+			->submit();
+
+		/* collect output */
+
+		$output .= $titleElement . $formElement;
+		$output .= Hook::trigger('loginEnd');
+		return $output;
+	}
+}
