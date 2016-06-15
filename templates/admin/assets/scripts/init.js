4771275994158529934e74e573cdb8900a77dd8a
redaxmedia <info@redaxmedia.com>
2016-06-15 22:02:12 +0200

    Move JS plugins to the templates

--- templates/admin/assets/scripts/init.js
+++ templates/admin/assets/scripts/init.js
@@ -1,7 +1,10 @@
 /**
  * @tableofcontents
  *
- * 1. init
+ * 1. dock
+ * 2. generate alias
+ * 3. generate keyword
+ * 4. panel
  *
  * @since 3.0.0
  *
@@ -9,4 +12,76 @@
  * @author Henry Ruhs
  */
 
-/* @section 1. init */
+/* @section 1. dock */
+
+rs.plugins.dock =
+{
+	init: true,
+	selector: 'div.rs-admin-js-dock',
+	options:
+	{
+		element:
+		{
+			dockLink: 'a.rs-admin-js-link-dock',
+			dockDescription: 'span.rs-admin-js-description-dock',
+			dockDescriptionHTML: '<span class="rs-admin-js-description-dock rs-admin-description-dock"></span>'
+		},
+		vibrate: 100
+	}
+};
+
+/* @section 2. generate alias */
+
+rs.plugins.generateAlias =
+{
+	init: true,
+	selector: 'form input.rs-admin-js-generate-alias-input, form input.rs-admin-js-generate-alias-output',
+	options:
+	{
+		element:
+		{
+			related: 'input.rs-admin-js-generate-alias-output'
+		}
+	}
+};
+
+/* @section 3. generate keyword */
+
+rs.plugins.generateKeyword =
+{
+	init: true,
+	selector: 'form textarea.rs-admin-js-generate-keyword-input',
+	options:
+	{
+		element:
+		{
+			related: 'textarea.rs-admin-js-generate-keyword-output',
+			target: 'h1, h2, h3, strong'
+		},
+		splitter:
+		{
+			text: '\n',
+			keyword: ' '
+		},
+		delimiter: ' ',
+		limit: 10
+	}
+};
+
+/* @section 4. panel */
+
+rs.plugins.panel =
+{
+	init: true,
+	selector: 'ul.rs-admin-js-list-panel',
+	options:
+	{
+		element:
+		{
+			panelItem: 'li.rs-admin-js-item-panel'
+		},
+		timeout: 1000,
+		duration: 200,
+		vibrate: 100
+	}
+};
