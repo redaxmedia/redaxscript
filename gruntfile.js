3fc7c52f41fee0baa9ee67c19c10a55d746bf669
redaxmedia <info@redaxmedia.com>
2016-11-16 23:56:38 +0100

    Extend the feature listing

--- gruntfile.js
+++ gruntfile.js
@@ -647,13 +647,25 @@ module.exports = function (grunt)
 			{
 				src:
 				[
+					'node_modules/redaxscript-flaticon/flaticon/design/cube.svg',
+					'node_modules/redaxscript-flaticon/flaticon/essential/alarm.svg',
 					'node_modules/redaxscript-flaticon/flaticon/essential/database-1.svg',
+					'node_modules/redaxscript-flaticon/flaticon/essential/document.svg',
+					'node_modules/redaxscript-flaticon/flaticon/essential/magic-wand.svg',
+					'node_modules/redaxscript-flaticon/flaticon/essential/paper-plane-1.svg',
+					'node_modules/redaxscript-flaticon/flaticon/essential/settings-6.svg',
+					'node_modules/redaxscript-flaticon/flaticon/essential/stopwatch-1.svg',
 					'node_modules/redaxscript-flaticon/flaticon/productivity-and-development/rocket-ship.svg',
 					'node_modules/redaxscript-flaticon/flaticon/online-marketing/devices.svg',
 					'node_modules/redaxscript-flaticon/flaticon/programming-line/command-line.svg',
+					'node_modules/redaxscript-flaticon/flaticon/programming-line/editing-code.svg',
 					'node_modules/redaxscript-flaticon/flaticon/science/atoms.svg',
 					'node_modules/redaxscript-flaticon/flaticon/science/flask.svg',
+					'node_modules/redaxscript-flaticon/flaticon/security/antivirus.svg',
+					'node_modules/redaxscript-flaticon/flaticon/security/key.svg',
+					'node_modules/redaxscript-flaticon/flaticon/seo-and-analytics/analytics-2.svg',
 					'node_modules/redaxscript-flaticon/flaticon/stationery/layers.svg',
+					'node_modules/redaxscript-flaticon/flaticon/stationery/paint-palette-1.svg',
 					'node_modules/redaxscript-flaticon/flaticon/travel/cruise.svg'
 				],
 				dest: 'templates/website/dist/fonts',
