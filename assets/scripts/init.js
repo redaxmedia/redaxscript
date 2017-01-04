4d7792dc756eddbd39a47478d652acd0f806ec24
redaxmedia <info@redaxmedia.com>
2017-01-04 15:57:33 +0100

    Lazy load the teaser image

--- assets/scripts/init.js
+++ assets/scripts/init.js
@@ -242,5 +242,16 @@
 				}
 			}
 		}
+
+		/* load */
+
+		win.onload = function ()
+		{
+			if (docElement.className)
+			{
+				docElement.className += ' ';
+			}
+			docElement.className += 'rs-is-load';
+		}
 	})();
 })(document, document.documentElement, window);
