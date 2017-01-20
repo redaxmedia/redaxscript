0e87e11cf03482ba2d96d0a85470c5afd4690560
redaxmedia <info@redaxmedia.com>
2017-01-20 19:54:02 +0100

    Remove the cameleon tempalte idea

--- gruntfile.js
+++ gruntfile.js
@@ -407,14 +407,6 @@ module.exports = function (grunt)
 				],
 				dest: 'templates/console/dist/styles/console.min.css'
 			},
-			templateCameleon:
-			{
-				src:
-				[
-					'templates/cameleon/assets/styles/_cameleon.css'
-				],
-				dest: 'templates/cameleon/dist/styles/cameleon.min.css'
-			},
 			templateDefault:
 			{
 				src:
