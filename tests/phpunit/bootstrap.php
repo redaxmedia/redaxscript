8ac63db1fae1f3f0b73fde4541405b8f51d7275f
redaxmedia <info@redaxmedia.com>
2017-08-27 00:50:11 +0200

    Update bootstrap.php

--- tests/phpunit/bootstrap.php
+++ tests/phpunit/bootstrap.php
@@ -3,12 +3,12 @@ namespace Redaxscript;
 
 error_reporting(E_DEPRECATED | E_WARNING | E_ERROR | E_PARSE);
 
-/* autoload */
+/* include autoload */
 
 include_once('includes' . DIRECTORY_SEPARATOR . 'Autoloader.php');
 include_once('TestCaseAbstract.php');
 
-/* deprecated */
+/* include deprecated */
 
 include_once('includes' . DIRECTORY_SEPARATOR . 'admin_admin.php');
 include_once('includes' . DIRECTORY_SEPARATOR . 'admin_list.php');
@@ -20,7 +20,7 @@ include_once('includes' . DIRECTORY_SEPARATOR . 'navigation.php');
 include_once('includes' . DIRECTORY_SEPARATOR . 'query.php');
 include_once('includes' . DIRECTORY_SEPARATOR . 'router.php');
 
-/* init */
+/* autoload */
 
 $autoloader = new Autoloader();
 $autoloader->init();
