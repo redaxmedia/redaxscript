c426f4bf75d285d64adc37b50d8806c2ea054835
redaxmedia <info@redaxmedia.com>
2018-09-21 18:38:23 +0200

    Basic table sorter part3

--- includes/Admin/View/CategoryTable.php
+++ includes/Admin/View/CategoryTable.php
@@ -148,7 +148,7 @@ class CategoryTable extends ViewAbstract
 						$tdElement
 							->copy()
 							->addClass('rs-admin-col-move')
-							->addClass($categoriesTotal > 1 && !$value->parent ? 'rs-admin-is-active' : null)
+							->addClass($categoriesTotal > 1 ? 'rs-admin-is-active' : null)
 							->text($value->rank)
 				);
 			}
